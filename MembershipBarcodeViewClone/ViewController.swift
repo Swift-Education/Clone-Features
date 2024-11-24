3a6b2164194b90aa08fa6c6eed3d577a13c97f61
kangddong <rkdehddud96@naver.com>
2024-11-24 19:55:34 +0900

    Initial Commit

---
+++ MembershipBarcodeViewClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MembershipBarcodeViewClone
+//
+//  Created by 강동영 on 11/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
